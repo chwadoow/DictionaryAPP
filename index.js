33709da1a1929694d0b4c0b08c4f0153a1124c65
moses ngige <moses32ngigz@gmail.com>
2022-06-22 15:37:17 +0300

    input .input padding changed

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const input = document.getElementById('input')
+const search_btn = document.getElementById('search_btn')
+const not_found=document.getElementById('not_found')
+const apiKey = '48d3b1d2-f6ec-41e4-81c8-ce914ebe2e72'
+const audio= document.querySelector('audio')
 
 
 
+search_btn.addEventListener('click',e=>{ 
+    e.preventDefault()
+
+    const word = input.value ;
+    if(word===''){
+        alert('Please type a word')
+        return;
+        
+    }
+    getTheData(word)
+})
+
+function getTheData(word){
+    fetch(`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${apiKey}`)
+        .then(res=>res.json())
+        .then(res=>{ 
+   
+            if(!res.length){
+                not_found.innerText='no results'
+            return;
+         }
+            
+            let definition =res[0].shortdef[0]         
+            not_found.innerText=definition
+
+            
+ })}
